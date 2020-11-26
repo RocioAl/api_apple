fd0e1b22a4590f5047e03f313b910790805a7ed4
Rocío Alberto <koroinice@gmail.com>
2020-11-25 21:07:24 -0500

    cambiando a puerto 4200

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 const bodyParser = require('body-parser');
 const { request, response } = require('express');
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 4200;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
     extended: true
