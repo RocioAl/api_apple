2985b7dfd7ad6397981f4b64a3e0c2a677858881
Rocío Alberto <koroinice@gmail.com>
2020-11-25 20:12:41 -0500

    se agregó cors

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
+
+
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const { request, response } = require('express');
-
+// Configurar cabeceras y cors
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+    res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
+    next();
+});
 const port = process.env.PORT || 5000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
