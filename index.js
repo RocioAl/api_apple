dfb555dba63d9f7dc8443ffa9182e7deb2c646b3
Rocío Alberto <koroinice@gmail.com>
2020-11-26 14:46:51 -0500

    agregando detalle del producto

--- index.js
+++ index.js
@@ -54,7 +54,82 @@ app.get('/products', (request, response) => {
             description: 'Ipad Mini'
         },
         {
+            id: '5',
+            title: 'Macbook',
+            price: 60000,
+            image: 'assets/images/macbook.png',
+            description: 'Macbook'
+        }]
+
+    )
+});
+app.get('/products/1', (request, response) => {
+
+
+    response.json(
+
+        [{
+            id: '1',
+            title: 'Apple-tv',
+            price: 60000,
+            image: 'assets/images/apple-tv.jpg',
+            description: 'Rosa Apple-tv piel'
+        }]
+
+    )
+});
+app.get('/products/2', (request, response) => {
+
+
+    response.json(
+
+        [{
+            id: '2',
+            title: 'Iphone',
+            price: 40000,
+            image: 'assets/images/iphone.jpg',
+            description: ' Iphonr Plus, 32 GB, oro rosa'
+        }]
+
+    )
+});
+app.get('/products/3', (request, response) => {
+
+
+    response.json(
+
+        [{
+            id: '3',
+            title: 'Homepod Mini',
+            price: 30000,
+            image: 'assets/images/homepod-mini.jpg',
+            description: 'Apple HomePod Speakers white'
+        }]
+
+    )
+});
+app.get('/products/4', (request, response) => {
+
+
+    response.json(
+
+        [{
             id: '4',
+            title: 'Ipad ',
+            price: 40000,
+            image: 'assets/images/ipad-mini.jpg',
+            description: 'Ipad Mini'
+        }]
+
+    )
+});
+app.get('/products/5', (request, response) => {
+
+
+    response.json(
+
+        [{
+            id: '5',
             title: 'Macbook',
             price: 60000,
             image: 'assets/images/macbook.png',
